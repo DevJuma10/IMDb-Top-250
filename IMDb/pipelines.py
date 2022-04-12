08ce2b01b9f910499a14b63e7f26be6e8c44f326
Juma Melvine <juma.study@gmail.com>
2022-04-12 23:12:15 +0300

    Generated SQLite Table populated with scraped data

--- IMDb/pipelines.py
+++ IMDb/pipelines.py
@@ -5,24 +5,46 @@
 
 
 # useful for handling different item types with a single interface
+from multiprocessing import connection
 from itemadapter import ItemAdapter
-import logging
-import pymongo
+import sqlite3
 
 
 
-
-class MongodbPipeline(object):
-    collection_name = "best movies"
-
+class SQLlitePipeline(object):
+    
     def open_spider(self, spider):
-        self.client = pymongo.MongoClient("mongodb+srv://Ibrahim:admin@cluster0.qgn4o.mongodb.net/IMDb?retryWrites=true&w=majority")
-        self.db = self.client["IMDb"]
+        self.connection = sqlite3.connect("IMDb.db")
+        self.c = self.connection.cursor()
+        self.c.execute('''
+        CREATE TABLE imdb_top_250(
+            title TEXT,
+            year TEXT,
+            duration TEXT,
+            genre TEXT,
+            rating TEXT,
+            movie_url TEXT
+        )
+        ''')
+
+        self.connection.commit()
 
     def close_spider(self, spider):
-        self.client.close()
+        self.connection.close()
 
 
     def process_item(self, item, spider):
-        self.db[self.collection_name].insert(item)
+        self.c.execute('''
+            INSERT INTO imdb_top_250(title, year, duration, genre, rating, movie_url) VALUES (?, ?, ?, ?, ?, ?)
+
+        ''', (
+            item.get("title"),
+            item.get("year"),
+            item.get("duration"),
+            item.get("genre"),
+            item.get("rating"),
+            item.get("movie_url")
+        ))
+
+        self.connection.commit()
         return item
