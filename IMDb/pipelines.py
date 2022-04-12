bb0da38e5ba1bd0980e90554a533e9fe9a628d1c
Juma Melvine <juma.study@gmail.com>
2022-04-12 14:45:31 +0300

    Working with MongoDb

--- IMDb/pipelines.py
+++ IMDb/pipelines.py
@@ -6,8 +6,23 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import logging
+import pymongo
 
 
-class ImdbPipeline:
+
+
+class MongodbPipeline(object):
+    collection_name = "best movies"
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient("mongodb+srv://Ibrahim:admin@cluster0.qgn4o.mongodb.net/IMDb?retryWrites=true&w=majority")
+        self.db = self.client["IMDb"]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert(item)
         return item
